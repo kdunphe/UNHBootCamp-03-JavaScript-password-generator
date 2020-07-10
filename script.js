926a4bfa45dac4573dd55b4146bf012a9da599b2
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-07-10 14:56:38 -0400

    Updating files.

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-// Assignment Code
+// Password Arrays
+const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '0123456789';
+const symbols = '`~!@#$%^&*()[]{}|\:;"_-+=./<>?'; 
+
+
+// Password Button Criteria
 const generateBtn = document.querySelector("#generate");
 
-// Password Parameters
-const lowercase = document.querySelector('#lowercase');
-const uppercase = document.querySelector('#uppercase');
-const number = document.querySelector('#number');
-const symbol = documemt.querySelector('#symbol');
+const lower = document.querySelector('#lowercase');
+const upper = document.querySelector('#uppercase');
+const num = document.querySelector('#numbers');
+const sym = document.querySelector('#symbols');
+
+
+// Generate Random Password
+function generatePassword () {
+}
+
+// If Statements
+
+// Validate Password - Loop?
+// if {length < 8 || > 128
+// alert()
+// return()
+// }
+// Math random & Math floor?
+
 
 
 // Write password to the #password input
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-//Generate Random Password
-
-function generatePassword () {
-
-  
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
