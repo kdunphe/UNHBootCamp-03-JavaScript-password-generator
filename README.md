e2552159377905b5b03fcb3e10ba197a124c6643
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-07-12 11:46:00 -0400

    Updating file.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # UNHBootCamp-03-JavaScript-password-generator
+
+**Homework Assignment #3**
+
+Create a random password generator based on user-selected criteria powered by JavaScript code.
+
+**The password generator was created and includes:**
+* A form with an input box for password length and checkboxes to select password criteria such as lowercase letters, uppercase letters, numbers, and symbols.
+* A clean, responsive design leveraging Bootstrap
+* Follows semantic HTML5
+* Incorporated Font Awesome icon
+
+_Link to deployed website:_ https://kdunphe.github.io/UNHBootCamp-03-JavaScript-password-generator/
+
+![AboutMe-Screentshot](/assets/AboutMe-Screenshot.jpg)
+![Portfolio-Screentshot](/assets/Portfolio-Screenshot.jpg)
+![Contact-Screentshot](/assets/Contact-Screenshot.jpg)
--- script.js
+++ script.js
@@ -3,9 +3,6 @@ const lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
 const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 const numbers = "0123456789".split("");
 const symbols = "~!@#$%^&*()[]{}|\:;_-+=./<>?".split("");
-// let pswdLength;
-// let pswdString;
-// let pswdCharSet;
 
 
 // Password Button Criteria
@@ -14,102 +11,80 @@ const isLowercase = document.querySelector("#lowercase");
 const isUppercase = document.querySelector("#uppercase");
 const isNumber = document.querySelector("#numbers");
 const isSymbol = document.querySelector("#symbols");
-// let pswdLength = document.querySelector("#length").value;
 let checkbox = document.querySelector("form");
-
-
-// Generate Random Password
-// function generatePassword () {
-
-//   let pswdString = "";
-//   let pswdCharSet = "";
-
-//   // Not working
-//   // let length = document.querySelector("#length").value;
-
-//   if (lower.checked) {
-//     pswdCharSet += pswdString.concat(lowercase);
-
-//   }
-
-//   if (upper.checked) {
-//     pswdCharSet += uppercase;
-
-//   }
-
-//   if (num.checked) {
-//     pswdCharSet += numbers;
-
-//   }
-
-//   if (sym.checked) {
-//     pswdCharSet += symbols;
-
-//   }
-
-//   // pswdLength = Number(length.value);
-
-//   for (i = 0; i < pswdLength; i++) {
-//     pswdString += pswdCharSet.charAt(Math.floor(Math.random() * pswdCharSet.length));
-
-//   }
-
-//   console.log(pswdCharSet);
-
-
-//   if (pswdString === "") {
-//     alert("Please select an input to generate a password.");
-//     return;
-// }
-// }
-
 
 
 // Generate Random Password
 function generatePassword() {
 
   // Source: https://www.w3schools.com/jsref/prop_text_value.asp
-    let pswdLength = document.querySelector("#length").value;
+  let pswdLength = document.querySelector("#length").value;
 
-    if (pswdLength < 8 || pswdLength > 128) {
-        alert("Please enter a number between 8 and 128.");
-        return "";
-    }
+  if (pswdLength < 8 || pswdLength > 128) {
+    alert("Please enter a number between 8 and 128.");
+    return "";
+  }
 
-    // Source: https://thisinterestsme.com/javascript-detect-checkbox-change/
-    // Source: https://stackoverflow.com/questions/28350387/display-alert-if-checkbox-is-unchecked-and-text-field-is-not-empty
-    // Needs tweaking - alerting regardless of checked/unchecked
-    // if (!checkbox.checked) {
-    //   alert("Please choose at least one character type.");
-    //   return "";
-    // }
+  // Source: https://thisinterestsme.com/javascript-detect-checkbox-change/
+  // Source: https://stackoverflow.com/questions/28350387/display-alert-if-checkbox-is-unchecked-and-text-field-is-not-empty
+  // Needs tweaking - alerting regardless of checked/unchecked
+  // if (!checkbox.checked) {
+  //   alert("Please choose at least one character type.");
+  //   return "";
+  // }
 
-    let pswdOption = [];
-    let pswdResult = [];
+  // if (!this.form.checkbox.checked) {
+  //   alert("Please choose at least one character type.");
+  //   return "";
 
-    // Selecting Character Types (Checkbox)
-    // Source: https://www.samanthaming.com/tidbits/49-2-ways-to-merge-arrays/
-    if (isLowercase.checked) {
-        pswdOption = pswdOption.concat(lowercase);
-    }
-    if (isUppercase.checked) {
-        pswdOption = pswdOption.concat(uppercase);
-    }
-    if (isNumber.checked) {
-        pswdOption = pswdOption.concat(numbers);
-    }
-    if (isSymbol.checked) {
-        pswdOption = pswdOption.concat(symbols);
-    }
+  // }
 
-    // Create Random Combination of Characters
-    // Source: https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-    for(i = 0; i < pswdLength; i++) {
-        pswdResult.push(pswdOption[Math.floor(Math.random() * pswdOption.length)]);
-    }
+    // if (checkbox.checked == false) {
+    //   return false;}
+    //   else {
+    //     alert("Please choose at least one character type.");
+    //   }
 
-    return pswdResult.join("");
+// Source: https://stackoverflow.com/questions/11234622/simple-javascript-checkbox-validation
+    if (
+      checkbox[0].checked == false &&
+      checkbox[1].checked == false &&
+      checkbox[2].checked == false &&
+      checkbox[3].checked == false
+    ) {
+      alert("Please choose at least one character type.");
+    } 
+
+
+
+  let pswdOption = [];
+  let pswdResult = [];
+
+  // Selecting Character Types (Checkbox)
+  // Source: https://www.samanthaming.com/tidbits/49-2-ways-to-merge-arrays/
+  if (isLowercase.checked) {
+    pswdOption = pswdOption.concat(lowercase);
+  }
+  if (isUppercase.checked) {
+    pswdOption = pswdOption.concat(uppercase);
+  }
+  if (isNumber.checked) {
+    pswdOption = pswdOption.concat(numbers);
+  }
+  if (isSymbol.checked) {
+    pswdOption = pswdOption.concat(symbols);
+  }
+
+  // Create Random Combination of Characters
+  // Source: https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+  for (i = 0; i < pswdLength; i++) {
+    pswdResult.push(pswdOption[Math.floor(Math.random() * pswdOption.length)]);
+  }
+
+  return pswdResult.join("");
 }
+
+
 
 // Write password to the #password input
 function writePassword() {
