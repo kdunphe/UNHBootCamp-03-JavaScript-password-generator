86efa91022e9f47ffe436e571334cb5027085176
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-07-12 10:51:08 -0400

    Updating files.

--- script.js
+++ script.js
@@ -1,76 +1,115 @@
 // Arrays
-const lowercase = 'abcdefghijklmnopqrstuvwxyz';
-const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const numbers = '0123456789';
-const symbols = '`~!@#$%^&*()[]{}|\:;"_-+=./<>?'; 
-let pswdLength;
-let pswdString;
-let pswdCharSet;
+const lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const numbers = "0123456789".split("");
+const symbols = "~!@#$%^&*()[]{}|\:;_-+=./<>?".split("");
+// let pswdLength;
+// let pswdString;
+// let pswdCharSet;
 
 
 // Password Button Criteria
 const generateBtn = document.querySelector("#generate");
-
-const lower = document.querySelector("#lowercase");
-const upper = document.querySelector("#uppercase");
-const num = document.querySelector("#numbers");
-const sym = document.querySelector("#symbols");
-let length = document.querySelector("#length");
+const isLowercase = document.querySelector("#lowercase");
+const isUppercase = document.querySelector("#uppercase");
+const isNumber = document.querySelector("#numbers");
+const isSymbol = document.querySelector("#symbols");
+// let pswdLength = document.querySelector("#length").value;
+let checkbox = document.querySelector("form");
 
 
 // Generate Random Password
-function generatePassword () {
+// function generatePassword () {
 
-  let pswdString = "";
-  let pswdCharSet = "";
+//   let pswdString = "";
+//   let pswdCharSet = "";
 
-  // Not working
-  // let length = document.querySelector("#length").value;
+//   // Not working
+//   // let length = document.querySelector("#length").value;
 
-  if (lower.checked) {
-    pswdCharSet += lowercase;
+//   if (lower.checked) {
+//     pswdCharSet += pswdString.concat(lowercase);
 
-  }
+//   }
 
-  if (upper.checked) {
-    pswdCharSet += uppercase;
+//   if (upper.checked) {
+//     pswdCharSet += uppercase;
 
-  }
+//   }
 
-  if (num.checked) {
-    pswdCharSet += numbers;
+//   if (num.checked) {
+//     pswdCharSet += numbers;
 
-  }
+//   }
 
-  if (sym.checked) {
-    pswdCharSet += symbols;
+//   if (sym.checked) {
+//     pswdCharSet += symbols;
 
-  }
+//   }
 
-  pswdLength = Number(length.value);
+//   // pswdLength = Number(length.value);
 
-  for (let i = 0; i < pswdLength; i++) {
-    pswdString += pswdCharSet.charAt(
-      Math.floor(Math.random() * pswdCharSet.length)
-    );
+//   for (i = 0; i < pswdLength; i++) {
+//     pswdString += pswdCharSet.charAt(Math.floor(Math.random() * pswdCharSet.length));
 
-  }
+//   }
 
-  console.log(pswdCharSet);
-}
+//   console.log(pswdCharSet);
 
 
-
-// If Statements
-
-// Validate Password - Loop?
-// if {length < 8 || > 128
-// alert()
-// return()
+//   if (pswdString === "") {
+//     alert("Please select an input to generate a password.");
+//     return;
 // }
-// Math random & Math floor?
+// }
 
 
+
+// Generate Random Password
+function generatePassword() {
+
+  // Source: https://www.w3schools.com/jsref/prop_text_value.asp
+    let pswdLength = document.querySelector("#length").value;
+
+    if (pswdLength < 8 || pswdLength > 128) {
+        alert("Please enter a number between 8 and 128.");
+        return "";
+    }
+
+    // Source: https://thisinterestsme.com/javascript-detect-checkbox-change/
+    // Source: https://stackoverflow.com/questions/28350387/display-alert-if-checkbox-is-unchecked-and-text-field-is-not-empty
+    // Needs tweaking - alerting regardless of checked/unchecked
+    // if (!checkbox.checked) {
+    //   alert("Please choose at least one character type.");
+    //   return "";
+    // }
+
+    let pswdOption = [];
+    let pswdResult = [];
+
+    // Selecting Character Types (Checkbox)
+    // Source: https://www.samanthaming.com/tidbits/49-2-ways-to-merge-arrays/
+    if (isLowercase.checked) {
+        pswdOption = pswdOption.concat(lowercase);
+    }
+    if (isUppercase.checked) {
+        pswdOption = pswdOption.concat(uppercase);
+    }
+    if (isNumber.checked) {
+        pswdOption = pswdOption.concat(numbers);
+    }
+    if (isSymbol.checked) {
+        pswdOption = pswdOption.concat(symbols);
+    }
+
+    // Create Random Combination of Characters
+    // Source: https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+    for(i = 0; i < pswdLength; i++) {
+        pswdResult.push(pswdOption[Math.floor(Math.random() * pswdOption.length)]);
+    }
+
+    return pswdResult.join("");
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -78,7 +117,6 @@ function writePassword() {
   let passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
